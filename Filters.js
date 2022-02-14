feb9bae05a5263db181c8345746b55be8a4503e4
Mamen code <72928405+mamencode@users.noreply.github.com>
2022-02-14 21:31:03 +0300

    Create Filters.js

---
+++ Filters.js
@@ -0,0 +1,110 @@
+const FILTERS = [
+  {
+    title: 'Normal',
+    filterComponent: AdenCompat,
+  },
+  {
+    title: 'Maven',
+    filterComponent: MavenCompat,
+  },
+  {
+    title: 'Mayfair',
+    filterComponent: MayfairCompat,
+  },
+  {
+    title: 'Moon',
+    filterComponent: MoonCompat,
+  },
+  {
+    title: 'Nashville',
+    filterComponent: NashvilleCompat,
+  },
+  {
+    title: 'Perpetua',
+    filterComponent: PerpetuaCompat,
+  },
+  {
+    title: 'Reyes',
+    filterComponent: ReyesCompat,
+  },
+  {
+    title: 'Rise',
+    filterComponent: RiseCompat,
+  },
+  {
+    title: 'Slumber',
+    filterComponent: SlumberCompat,
+  },
+  {
+    title: 'Stinson',
+    filterComponent: StinsonCompat,
+  },
+  {
+    title: 'Brooklyn',
+    filterComponent: BrooklynCompat,
+  },
+  {
+    title: 'Earlybird',
+    filterComponent: EarlybirdCompat,
+  },
+  {
+    title: 'Clarendon',
+    filterComponent: ClarendonCompat,
+  },
+  {
+    title: 'Gingham',
+    filterComponent: GinghamCompat,
+  },
+  {
+    title: 'Hudson',
+    filterComponent: HudsonCompat,
+  },
+  {
+    title: 'Inkwell',
+    filterComponent: InkwellCompat,
+  },
+  {
+    title: 'Kelvin',
+    filterComponent: KelvinCompat,
+  },
+  {
+    title: 'Lark',
+    filterComponent: LarkCompat,
+  },
+  {
+    title: 'Lofi',
+    filterComponent: LofiCompat,
+  },
+  {
+    title: 'Toaster',
+    filterComponent: ToasterCompat,
+  },
+  {
+    title: 'Valencia',
+    filterComponent: ValenciaCompat,
+  },
+  {
+    title: 'Walden',
+    filterComponent: WaldenCompat,
+  },
+  {
+    title: 'Willow',
+    filterComponent: WillowCompat,
+  },
+  {
+    title: 'Xpro2',
+    filterComponent: Xpro2Compat,
+  },
+  {
+    title: 'Aden',
+    filterComponent: AdenCompat,
+  },
+  {
+    title: '_1977',
+    filterComponent: _1977Compat,
+  },
+  {
+    title: 'Brannan',
+    filterComponent: BrannanCompat,
+  },
+];
